4d5f21aba249e355f5a9288c9f902720addfebe4
Sebastian Pipping <sebastian@pipping.org>
2020-07-25 03:34:10 +0200

    tests: Add essential system tests

--- wait_for_it/test_wait_for_it.py
+++ wait_for_it/test_wait_for_it.py
@@ -1,8 +1,52 @@
 """wait_for_it cli test module"""
+import socket
+import socketserver
+from threading import Event, Thread
 from unittest import TestCase
 
 from click.testing import CliRunner
 from .wait_for_it import cli
+
+_ANY_FREE_PORT = 0
+
+
+class _DummyTcpServerThread(Thread):
+    """
+    A TCP server as a Thread that takes any free port, accepts connections,
+    but doesn't do any meaningful traffic.
+    """
+
+    class _DummyHandler(socketserver.BaseRequestHandler):
+        pass
+
+    def __init__(self):
+        super().__init__()
+        self.started = Event()
+
+    def run(self):
+        with socketserver.TCPServer(
+            ("127.0.0.1", _ANY_FREE_PORT), self._DummyHandler
+        ) as self._server:
+            self.host, self.port = self._server.server_address
+            self.started.set()
+            self._server.serve_forever()
+
+    def stop(self):
+        self._server.shutdown()
+
+
+def _start_server_thread():
+    server = _DummyTcpServerThread()
+    server.start()
+    server.started.wait()
+    return server
+
+
+def _occupy_free_tcp_port():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(("127.0.0.1", _ANY_FREE_PORT))
+    host, port = sock.getsockname()
+    return host, port, sock
 
 
 class CliTest(TestCase):
@@ -13,5 +57,25 @@ class CliTest(TestCase):
 
     def test_help(self):
         result = self._runner.invoke(cli, ["--help"])
-        assert 'Usage:' in result.output
+        assert "Usage:" in result.output
         assert result.exit_code == 0
+
+    def test_service_available(self):
+        server = _start_server_thread()
+        try:
+            result = self._runner.invoke(
+                cli, ["-t1", "-s", f"{server.host}:{server.port}"]
+            )
+            assert " is available after " in result.output
+            assert result.exit_code == 0
+        finally:
+            server.stop()
+
+    def test_service_unavailable(self):
+        host, port, sock = _occupy_free_tcp_port()
+        try:
+            result = self._runner.invoke(cli, ["-t1", "-s", f"{host}:{port}"])
+            assert "timeout occurred" in result.output
+            assert result.exit_code == 1
+        finally:
+            sock.close()
