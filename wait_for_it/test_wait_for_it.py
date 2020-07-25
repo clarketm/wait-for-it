4c0ce3727167b761982a3dae74374e973ff14323
Sebastian Pipping <sebastian@pipping.org>
2020-07-25 13:31:19 +0200

    tests: Use relative import

--- wait_for_it/test_wait_for_it.py
+++ wait_for_it/test_wait_for_it.py
@@ -1,6 +1,6 @@
 """wait_for_it cli test module"""
 from click.testing import CliRunner
-from wait_for_it.wait_for_it import cli
+from .wait_for_it import cli
 
 
 # https://click.palletsprojects.com/en/7.x/testing/
