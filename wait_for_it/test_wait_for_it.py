c95ca5f165adb6fac0ba0eab9640742611f24c9c
Sebastian Pipping <sebastian@pipping.org>
2020-07-25 13:35:24 +0200

    tests: Turn test_cli_help into a class for upcoming extension

--- wait_for_it/test_wait_for_it.py
+++ wait_for_it/test_wait_for_it.py
@@ -1,11 +1,17 @@
 """wait_for_it cli test module"""
+from unittest import TestCase
+
 from click.testing import CliRunner
 from .wait_for_it import cli
 
 
-# https://click.palletsprojects.com/en/7.x/testing/
-def test_cli_help():
-    runner = CliRunner()
-    result = runner.invoke(cli, ["--help"])
-    assert 'Usage:' in result.output
-    assert result.exit_code == 0
+class CliTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # https://click.palletsprojects.com/en/7.x/testing/
+        cls._runner = CliRunner()
+
+    def test_help(self):
+        result = self._runner.invoke(cli, ["--help"])
+        assert 'Usage:' in result.output
+        assert result.exit_code == 0
